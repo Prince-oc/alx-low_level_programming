41aea8885b5d64be16c90ee31bdfe40c40d10d23
Prince-oc <ocbazunu@gmail.com>
2023-07-04 17:50:15 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * *_memset(char *s, char b, unsigned int n)- a function that fills memory with a constant byte.
- * @n; byte of memory 
- * @s;memory area 
- * @b;constant byte
+ *_memset - fills memory with a constant byte
+ *@s: pointer to variable s
+ *@b: constant byte
+ *@n: n bytes we want to reeplace in variable c
  *
- * return:s
+ *Return: s
  */
- char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0; i < n; i++)
-	{
-
-		s[i]=b;
-	}
-
+		for (i = 0; i < n; i++)
+		{
+			s[i] = b;
+		}
+	return (s);
 }
-	
- 
- 
-
-
