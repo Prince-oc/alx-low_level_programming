d3f581edf11118bb81a572b82fd174279719ac82
Prince-oc <ocbazunu@gmail.com>
2023-07-06 16:49:43 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** _print_rev_recursion- prints a string in reverse.
+ * @s; spring to print 
+ * Return; nothing to return
+ *
+ void _print_rev_recursion(char *s);
+ {
+              if(*s! ='\0');
+	      {
+
+	      }
+	      {_print_rev_recursion(s + 1)
+	     
+	      }
+	      {  _putchar(s(0))
+
+    }
+     endif(s(0))
+ }
+    
+   
+	
