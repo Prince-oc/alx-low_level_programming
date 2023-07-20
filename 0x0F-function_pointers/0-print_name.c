c3fda5696b874b49e36c3ebdeee36cb03ac6c2b1
Prince-oc <ocbazunu@gmail.com>
2023-07-20 12:00:58 -0700

    function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+
+ print_name-function that prints a name.
+ @name:name to be print 
+ @f:pointer to function 
+
+ Return:nothing 
+
+ void print_name(char *name, void (*f)(char *));
+{
+	if (f != NULL)
+		f(name);
+}
